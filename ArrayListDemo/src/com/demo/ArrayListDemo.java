c9a3b483df57425bca2730e0b5c590618ea54d64
Rushikesh786 <rushikeshhiwanj@gmail.com>
2022-10-21 00:40:37 +0530

    ArrayList program is added

---
+++ ArrayListDemo/src/com/demo/ArrayListDemo.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+		List<String> list = Arrays.asList("Ram","Pooja","Shivam","Omkar");
+		System.out.println(list);
+	}
+
+}
